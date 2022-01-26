25cf7447b3f6dc43c536e847d70e83eba2b2a35c
Atefeh <atefeh@buzzjective.com>
2022-01-26 12:26:15 +0330

    config headers of the request

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import axios from 'axios';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'https://api.realworld.io/api/';
+axios.defaults.headers.post['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 new Vue({
   router,
